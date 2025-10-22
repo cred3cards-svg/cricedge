1502cc387306087a400ffbb4e8b0fdc7b17ce343
cred3cards <cred3cards@gmail.com>
2025-10-22 17:31:37 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1832356161-758ad",
+  "appId": "1:893485644229:web:cb6ce57e671d55e20533ac",
+  "apiKey": "AIzaSyAfGjL-GMf0EaZR1skb08YTlX9Hx3sZ88o",
+  "authDomain": "studio-1832356161-758ad.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "893485644229"
+};
